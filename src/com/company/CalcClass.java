8b5f492a081767920b8cbcdd54d9341453e7ed51
abbeyrcallow <abbeyrcallow@gmail.com>
2019-03-28 10:56:13 -0500

    new updated showMenu();

--- src/com/company/CalcClass.java
+++ src/com/company/CalcClass.java
@@ -2,9 +2,9 @@ package com.company;
 import java.util.Scanner;
 
 public class  CalcClass {
-   double num1;
-   double num2;
-   double num3;
+    double num1;
+    double num2;
+    double num3;
 
     public void showMenu() {
 
@@ -13,11 +13,11 @@ public class  CalcClass {
         Scanner reader = new Scanner(System.in);
         System.out.print("Please enter 3 numbers: ");
 
-         num1 = reader.nextInt();
-         num2 = reader.nextInt();
-         num3 = reader.nextInt();
+        num1 = reader.nextInt();
+        num2 = reader.nextInt();
+        num3 = reader.nextInt();
 
-         do {
+        do {
             System.out.println("Enter an operator:");
 
             System.out.println(" + ");
@@ -75,7 +75,7 @@ public class  CalcClass {
         System.out.println("Your answer is " + total);
     }
 
-    public void sub(){
+    public void sub() {
         double total = num1 - num2 - num3;
         System.out.println("Your answer is " + total);
     }
@@ -84,70 +84,14 @@ public class  CalcClass {
         double total = num1 * num2 * num3;
         System.out.println("Your answer is " + total);
     }
+
     public void div() {
         double total = num1 / num2 / num3;
         System.out.println("your answer is " + total);
     }
-    public void startOver(){
-        String user;
-        //System.out.println("Welcome to your calculator.");
-        Scanner reader = new Scanner(System.in);
-        System.out.print("Please enter 3 new numbers: ");
 
-        num1 = reader.nextInt();
-        num2 = reader.nextInt();
-        num3 = reader.nextInt();
-
-        do {
-            System.out.println("Enter an operator:");
-
-            System.out.println(" + ");
-
-            System.out.println(" - ");
-
-            System.out.println(" * ");
-
-            System.out.println(" /  ");
-
-            System.out.println("Press & to start over.");
-
-            System.out.println("Press # to exit calculator.");
-
-            System.out.println();
-
-
-            Scanner scanner = new Scanner(System.in);
-
-            user = scanner.nextLine();
-
-            switch (user.toString()) {
-
-                case "+":
-                    add();
-
-                    break;
-
-                case "-":
-                    sub();
-
-                    break;
-
-                case "*":
-                    mult();
-                    break;
-
-                case "/":
-                    div();
-                    break;
-
-                case "&":
-                    startOver();
-
-                case "#":
-                    break;
-            }
-
-        } while (!user.equals("#"));
+    public void startOver() {
+        showMenu();
     }
 }
 
